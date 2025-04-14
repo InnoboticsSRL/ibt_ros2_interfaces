0344cd144f857015dc4d49b80c1ddeab3eff2a48
Mattia Dei Rossi <mattydr98@gmail.com>
2025-04-14 11:32:40 +0200

    refactor IO service

---
+++ .github/actions/build_deb/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Create a .deb package
+rosdep update && \
+rosdep install --from-paths . --ignore-src -r -y --rosdistro=humble && \
+pip install . && \
+source /opt/ros/humble/setup.sh && \
+bloom-generate rosdebian && \
+fakeroot debian/rules binary
+
+# Move any generated .deb packages to the GitHub workspace
+mkdir -p /github/workspace/deb
+mv ../*.deb /github/workspace/deb/
